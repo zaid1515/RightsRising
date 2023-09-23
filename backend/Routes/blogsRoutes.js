45051948d061ea0a5bdfdca57ae92736401ad5e1
karan adwani <karanadwani.official@gmail.com>
2023-09-24 01:25:21 +0530

    multer installed

---
+++ backend/Routes/blogsRoutes.js
@@ -0,0 +1,8 @@
+const Router = require('express').Router();
+
+/*
+@Desc: to create/post a new blog.
+*/
+Router.post('/create');
+
+module.exports = Router;
---
+++ backend/controllers/blogsController.js
@@ -0,0 +1,11 @@
+const asyncHandler = require('express-async-handler');
+
+/*
+@Desc : to create/ post a new blog
+@route : POST /api/blogs/create
+@access : private
+*/
+
+const createBlog = asyncHandler( async(req, res) => {
+    
+})
