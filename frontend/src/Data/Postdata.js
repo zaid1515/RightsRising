d9841be418a11c681c24342c4545358c6a162224
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-09-24 20:38:13 +0530

    images videos added

---
+++ frontend/src/Data/Postdata.js
@@ -0,0 +1,62 @@
+const Postdata = [
+    {
+        id: 1,
+        name: "Vighnesh Adelkar",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 100,
+        userId: 1,
+        comments: 50
+    },
+    {
+        id: 2,
+        name: "Anton George",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 10,
+        userId: 10,
+        comments: 7
+    },
+    {
+        id: 3,
+        name: "Abuzaid Ansari",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes:6,
+        userId: 12,
+        comments: 43
+    },
+    {
+        id: 4,
+        name: "Kanav Bhatia",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 9,
+        userId: 3,
+        comments: 52
+    },
+    {
+        id: 5,
+        name: "Maanav Chellani",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 10,
+        userId: 8,
+        comments: 55
+    },
+    {
+        id: 6,
+        name: "Ruturaj Chandgude",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 14,
+        userId: 19,
+        comments: 58
+    },
+    {
+        id: 7,
+        name: "Vighnesh Adelkar",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the,1500s, when an unknown printer took a galley of type and scrambled it to, make a type specimen book.It has survived not only five centuries, but,also the leap into electronic typesetting, remaining essentially",
+        likes: 10,
+        userId: 1,
+        comments: 5
+    }
+]
+
+export default Postdata;
+
+
