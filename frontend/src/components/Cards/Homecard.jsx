ecb16373494b2f0fe2341215c63691eda974d39c
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-09-28 22:23:16 +0530

    changes in homecard

--- frontend/src/components/Cards/Homecard.jsx
+++ frontend/src/components/Cards/Homecard.jsx
@@ -4,17 +4,16 @@ import img1 from "../../images/bg4.jpg";
 
 export default function Homecard(props) {
   return (
-    <div className={props.ct}>
+    <div className={props.ct} id='hmcard'>
       <div className="cd6Wrapper">
         <div className="cd6textDiv">
           <h3>{props.name}</h3>
           <p className="cd6Text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
-            optio non unde temporibus velit necessitatibus odio.
+            {props.text}
           </p>
         </div>
         <div className="cd6imgDiv">
-          <img src={img1} alt="" className="cd6Image" />
+          <img src={props.image} alt="" className="cd6Image" />
         </div>
       </div>
     </div>
